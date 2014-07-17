93713aad2322e7603d3994b6a53d24956c31b041
Eric Roberts <ericroberts@gmail.com>
2014-07-16 20:23:06 -0400

    Create services page and setup path to it.

--- config.rb
+++ config.rb
@@ -25,6 +25,8 @@ require 'slim'
 #   page "/admin/*"
 # end
 
+proxy "/services", "/services.html"
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
